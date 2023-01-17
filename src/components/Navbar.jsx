2a9f0c227ba33c604bc9cf6564ca9fc2c2198684
kanishk <kanishkkashyap840@gmail.com>
2023-01-18 04:39:07 +0530

    New UI Implemented + Search Login Button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,106 +1,90 @@
 import React from "react";
-import styled from "styled-components";
-
+import styled, {css} from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
 import { Link, useLocation } from "react-router-dom";
 
+//  Main Container CSS
 const Container = styled.div`
-  position: fixed;
   display: flex;
-  border: 1px solid black;
-  justify-content: space-between;
-  align-items: center;
-  flex-wrap: wrap;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 1;
-`;
-const Left = styled.h1`
-  padding-left: 10px;
-  margin: 0;
+  flex-direction: column;
 `;
 
-const Span = styled.span`
-  color: red;
-  font-size: 2em;
-  padding: 0;
-  margin: 0;
-`;
-
-const Right = styled.div``;
-const Center = styled.div``;
-
-const NavItems = styled.div`
+// Top of Navbar Starts Here
+const Top = styled.div`
   display: flex;
   justify-content: space-between;
-  @media (max-width: 380px) {
-    flex-direction: column;
+  margin-top: 25px;
+  margin-right: 10px;
+  margin-left: 10px;
+`;
+
+const Logo = styled.img`
+  border-radius: 40%;
+  width: 50px;
+`;
+
+const TopRight = styled.div`
+  display: flex;
+  
+`;
+const SearchContainer = styled.div`
+  position: relative;
+`;
+
+const Search = styled.input`
+  border-radius: 15px;
+  
+  width: 500px;
+  height: 2.4rem;
+  background-color: #702222;
+  text-decoration: none;
+  border: none;
+  &:active {
+    border: none;
+  }
+
+  @media (max-width: 800px) {
+    
+    width: 200px;
   }
 `;
-const Projects = styled.h3`
-  margin-right: 20px;
+
+const ButtonContainer = styled.div`
+margin-left: 30px;
+
+margin-right: 30px;
+
+`;
+const Button = styled.button`
+width: 6em;
+ height: 2.4rem;
+  border-radius: 20px;
+  font-size: 1rem;
+  font-weight: 200;
+  background-color: white;
+  border: none;
+  padding: 10px;
+  margin-right: 8px ;
+  transition: all 0.1s ease-in;
   cursor: pointer;
-  &:hover {
-    color: #f90000;
-    transition: 700ms;
-  }
+ 
+  
+  &:hover{
+    font-size: 1.3rem;
 
-  &::after {
-    content: "";
-    width: 0px;
-    height: 5px;
-    display: block;
-    background: #ff0000;
-    transition: 500ms;
-    border-radius: 5px;
-  }
-
-  &.active::after {
-    width: 100%;
   }
 `;
-const Skills = styled.h3`
-  margin-right: 20px;
 
-  cursor: pointer;
-  &::after {
-    content: "";
-    width: 0px;
-    height: 5px;
-    display: block;
-    background: #f90000;
-    transition: 300ms;
-    border-radius: 5px;
-  }
-  &:hover {
-    color: #f90000;
-    transition: 700ms;
-  }
-
-  &.active::after {
-    width: 100%;
-  }
+// Top Ends Here
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-evenly;
 `;
-const ContactMe = styled.h3`
-  margin-right: 20px;
-  &:hover {
-    color: #f90000;
-    transition: 700ms;
-  }
-  cursor: pointer;
-  &::after {
-    content: "";
-    width: 0px;
-    height: 5px;
-    display: block;
-    background: #ff0000;
-    transition: 300ms;
-    border-radius: 5px;
-  }
 
-  &.active::after {
-    width: 100%;
-  }
+const NavItems = styled.a`
+  color: white;
+  text-decoration: none;
+  font-size: 1.2rem;
 `;
 
 const Navbar = () => {
@@ -108,36 +92,26 @@ const Navbar = () => {
 
   return (
     <Container>
-      <Right></Right>
-      <Left>
-        DoubtFree<Span>.</Span>{" "}
-      </Left>
-      <Center>
-        <NavItems>
-          <Link to="/" className="Link">
-            {" "}
-            <Projects className={url.pathname === "/" ? "active" : ""}>
-                Login
-            </Projects>
-          </Link>
-          <Link to="/projects" className="Link">
-            {" "}
-            <Projects className={url.pathname === "/projects" ? "active" : ""}>
-              Register
-            </Projects>
-          </Link>
-          <Link to="/contacts" className="Link">
-            <ContactMe className={url.pathname === "/contacts" ? "active" : ""}>
-              Contact
-            </ContactMe>
-          </Link>
-          <Link to="/skills" className="Link">
-            <Skills className={url.pathname === "/skills" ? "active" : ""}>
-              About Us
-            </Skills>
-          </Link>
-        </NavItems>
-      </Center>
+      <Top>
+        <Logo src="https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+        <TopRight>
+          <SearchContainer>
+            <Search /> <SearchIcon className="icon" />
+          </SearchContainer>
+          <ButtonContainer>
+            <Button>Login</Button>
+            <Button>Sighn Up</Button>
+          </ButtonContainer>
+        </TopRight>
+      </Top>
+      <Bottom>
+        <NavItems href="www.goog.com">HOME</NavItems>
+        <NavItems href="www.goog.com">IDEA VALIDATION</NavItems>
+        <NavItems href="www.goog.com">COMPANY FORMATION</NavItems>
+        <NavItems href="www.goog.com">MENTORING & INCUBATION</NavItems>
+        <NavItems href="www.goog.com">TECHINCAL & MARKETING </NavItems>
+        <NavItems href="www.goog.com">FUNDING & BANKING </NavItems>
+      </Bottom>
     </Container>
   );
 };
